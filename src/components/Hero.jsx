4af615c0afd2d663fe14208bfbb890b9ce86b651
Bruno Sanchez <sanchezbruno689@gmail.com>
2026-02-12 12:16:46 -0300

    Refactor Hero component structure: update section IDs for clarity and improve semantic HTML by replacing fragment with a section element.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -66,8 +66,8 @@ const Hero = () => {
         }
     }, [])
     return (
-        <>
-            <section id="home" className="noisy">
+        <section id="home">
+            <section id="hero" className="noisy">
                 <h1 className="title">MOJITO</h1>
                 <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf" />
                 <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf" />
@@ -96,7 +96,7 @@ const Hero = () => {
             <div className="video absolute inset-0">
                 <video ref={videoRef} src="/videos/output.mp4" playsinline preload="auto" muted></video>
             </div>
-        </>
+        </section>
     )
 }
 
